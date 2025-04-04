3c376e7fc8c086a4fb8638094bfa5dd2de62069b
HannesPrinsloo <johannespprinsloo@gmail.com>
2025-04-04 20:49:43 +0200

    fCC tests all passing

--- src/App.js
+++ src/App.js
@@ -1,25 +1,85 @@
-import logo from './logo.svg';
+import { marked } from 'marked';
+import { useState } from 'react';
+import React from 'react';
 import './App.css';
 
-function App() {
+marked.setOptions({
+  breaks: true,
+});
+
+
+const initialMD = `# Markdown Previewer
+
+## This is a sub-heading.
+
+Here 👉 \`const inlineCode = () => console.log(" is some inline code 🤘 ")\`
+
+Here is some block code: 
+
+\`\`\`javascript
+function add(a, b) {
+  return a + b;
+}
+\`\`\`
+
+Here is a list:
+
+- Item 1
+- Item 2
+- Item 3
+
+Here is a link: [freeCodeCamp](https://www.freecodecamp.org)
+
+> This is a blockquote.
+
+Here is some **bold** text.
+
+Here is some *italic* text.
+
+Here is a picture of Pikachu:
+
+![Pikachu](https://upload.wikimedia.org/wikipedia/en/a/a6/Pok%C3%A9mon_Pikachu_art.png)
+
+
+**This is bold text.**
+`; 
+
+function Markdown({ onInputChange }) {
+  const [postContent, setPostContent] = useState(initialMD);
+
+  const handleChange = (event) => {
+    setPostContent(event.target.value);
+    onInputChange(event.target.value); // Call the function passed from App
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <textarea
+      id="editor"
+      value={postContent}
+      onChange={handleChange}
+    />
   );
 }
 
-export default App;
+function Preview({ markdown }) {
+  const renderedHTML = marked.parse(markdown);
+  return (
+    <div id="preview" dangerouslySetInnerHTML={{ __html: renderedHTML }}></div>
+  );
+}
+
+export default function App() {
+  const [markdownInput, setMarkdownInput] = useState(initialMD);
+
+  const handleInputChange = (markdown) => {
+    setMarkdownInput(markdown);
+  };
+
+  return (
+    <div className="App">
+      <h1 className="App-title">Markdown Previewer</h1>
+      <Markdown onInputChange={handleInputChange} />
+      <Preview markdown={markdownInput} />
+    </div>
+  );
+}
